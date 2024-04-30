3721f03f07450da8c5d0ac1ebfb1097acba4ff8c
thiago-santanna <uzmaki_thiago@outlook.com>
2024-04-30 11:55:46 -0300

    Feature - Adicionando o starter da lib

---
+++ liquibase-sample/src/main/java/com/webapps/tss/liquibasesample/LiquibaseSampleApplication.java
@@ -0,0 +1,13 @@
+package com.webapps.tss.liquibasesample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiquibaseSampleApplication.class, args);
+    }
+
+}
---
+++ liquibase-sample/src/test/java/com/webapps/tss/liquibasesample/LiquibaseSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapps.tss.liquibasesample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquibaseSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
